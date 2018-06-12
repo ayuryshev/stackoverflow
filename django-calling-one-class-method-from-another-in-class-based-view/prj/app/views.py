7b30f6bf399cd79721f96529de682bbdf87e5d78
Alexander Yuryshev <alexander.yuryshev@gmail.com>
2018-06-12 06:07:31 +0300

    django==1.11

--- django-calling-one-class-method-from-another-in-class-based-view/prj/app/views.py
+++ django-calling-one-class-method-from-another-in-class-based-view/prj/app/views.py
@@ -1,42 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.utils.decorators import classonlymethod
 
 
-def hello_fn(request):
-    context = dict(
-        method="hello_fn",
-        player_id=1,
-        stats = 1,
-    )
+# CASE 1: normal function - OK
+def hello_fun(request):
+    context = dict(method="hello_fun")
     return render(request, 'hello.html', context)
 
+
 class HelloClass(object):
+    def get(self, request, **kwargs):
+        return render(request, 'hello.html', kwargs)
+
+    # Object method
+    def om_hello(self, request):
+        return self.get(request, 'hello.html', **{"method": "object method"})
+
     @classmethod
-    def hello(self, request):
-        context = dict(
-            method="HelloClass.hello",
-            player_id=1,
-            stats = 1,
-        )     
-        
-        return render(request, 'hello.html', context)
+    def cm_hello(cls, request):
+        return cls.get(cls, request, **{"method": "class method"})
 
-    def hello2(self, request):
-        context = dict(
-            method="HelloClass.hello",
-            player_id=1,
-            stats = 1,
-        )        
-        return render(request, 'hello.html', context)
+    @staticmethod
+    def sm_hello(request):
+        self = None  # Problem
+        return self.get(self, request, **{"method": "static method"})
+
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        def view(request, **kwargs):
+            self = cls(**initkwargs)
+            self.request = request
+            self.kwargs = kwargs
+            return self.get(request, **{"method": "view construction"})
+
+        return view
 
 
+class ScoreView(TemplateView):
+    template_name = "hello.html"
 
-class ScoreView(TemplateView):    
-    template_name="hello.html"
-
-    def get_context_data(self,**kwargs):        
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['method'] = "get_content_data"
         context['player_id'] = 1
@@ -44,13 +48,13 @@ class ScoreView(TemplateView):
         return context
 
     @classmethod
-    def get_player_stats(cls, request):        
-        context = {            
-            "method":"get_player_stats",
+    def get_player_stats(cls, request):
+        context = {
+            "method": "get_player_stats",
             "player_id": 2,
-            "stats": cls.get_team_stats(cls,2)
-        }        
+            "stats": cls.get_team_stats(cls, 2)
+        }
         return render(request, 'hello.html', context)
 
     def get_team_stats(self, player_id):
-        return player_id*2
+        return player_id * 2
--- django-calling-one-class-method-from-another-in-class-based-view/prj/prj/urls.py
+++ django-calling-one-class-method-from-another-in-class-based-view/prj/prj/urls.py
@@ -1,28 +1,16 @@
-"""prj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+    
+"""
+from django.conf.urls import url
 from django.contrib import admin
 
 import app.views
 # import app.views
 
 urlpatterns = [
-    path("hello/fn", app.views.hello_fn),
-    path("hello/class", app.views.HelloClass.hello),
-    path("hello/class2", app.views.HelloClass.hello2),
-    path('score/view', app.views.ScoreView.as_view()), # Intended use
-    path('score/get_player_stats', app.views.ScoreView.get_player_stats), # Noto
+    url("hello/fun", app.views.hello_fun),  # OK
+    url("hello/object_method", app.views.HelloClass.om_hello),  # Problem 
+    url("hello/class_method", app.views.HelloClass.cm_hello),  # OK
+    url('hello/static_method', app.views.HelloClass.sm_hello),  # Problem 
+    url('hello/as_view', app.views.HelloClass.as_view()),  # Noto
 ]
